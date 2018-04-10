52816f7320d30ee8b4583246a0768f6d82e193cc
Rodrigo Canaan <rodrigocanaan@Rodrigos-MacBook-Pro.local>
2018-04-09 22:22:39 -0400

    fixed last commit by adding audiocontroller file

---
+++ Assets/PlayerAudioController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudioController : MonoBehaviour {
+
+	public AudioClip hitSound;
+	public AudioClip deathSound;
+
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlaySound(string s){
+		if (s == "Hit"){
+			audioSource.clip = hitSound;
+			audioSource.Play();
+		}
+		if (s == "Death") {
+			audioSource.clip = deathSound;
+			audioSource.Play();
+		}
+	}
+}
