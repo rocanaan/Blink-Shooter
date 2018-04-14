f29754bb217df46c65e70341bace5d90917e8405
Rodrigo Canaan <rodrigocanaan@rodrigos-mbp.home>
2018-04-13 21:15:35 -0400

    Implemented laser prototype at scene Test Laser Attack

---
+++ Assets/_Scripts/Player/Player and Character Controllers/PhaseCharacterBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhaseCharacterBehavior : CharacterBehavior {
+
+	public GameObject bodyObject;
+	public GameObject ghostObject;
+	private GhostController ghostController;
+
+	public float moveSpeed;
+	public float speedWhileFiring;
+	public float shotInterval;
+	private float nextShot;
+
+
+
+	// Use this for initialization
+	void Start () {
+		ghostController = ghostObject.GetComponent<GhostController> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
