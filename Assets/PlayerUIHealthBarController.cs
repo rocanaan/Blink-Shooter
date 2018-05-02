58fc190b57d4f9ab3bc51deb7159534d2b860c6f
brkdncr94 <bd1308@nyu.edu>
2018-05-01 22:26:22 -0400

    Visual Feedback Commit
    -Player health bars on the side are now the same color as the player
    bodies.
    -Player health bars on the side blink red when health is low.
    -Damage taken affects the size of explosion now.
    -Minions have slightly darker color.

--- Assets/PlayerUIHealthBarController.cs
+++ Assets/PlayerUIHealthBarController.cs
@@ -7,9 +7,12 @@ public class PlayerUIHealthBarController : MonoBehaviour {
 
     public GameObject playerBody;
     public Image fill;
+    public Material highHealthMaterial;
+    public Material lowHealthMaterial;
 
     private HealthController playerHealthController;
     private Slider healthBar;
+    private bool healthLow;
 
 	// Use this for initialization
 	void Start () {
@@ -18,11 +21,52 @@ public class PlayerUIHealthBarController : MonoBehaviour {
 
         healthBar.maxValue = playerHealthController.maxHealth;
         healthBar.value = playerHealthController.healthBar.value;
+        healthLow = false;
+        fill.color = highHealthMaterial.color;
     }
 	
 	// Update is called once per frame
 	void Update () {
         healthBar.value = playerHealthController.healthBar.value;
-        fill.color = playerHealthController.fill.color;
+        //fill.color = playerHealthController.fill.color;
+        if (healthBar.value <= Mathf.RoundToInt(healthBar.maxValue / 3)) // if regenerating above the "danger" threshhold, health bar goes green
+        {
+            //start blinking
+            healthLow = true;
+            StartCoroutine(Blink());
+        }
+        else
+        {
+            healthLow = false;
+            StopCoroutine(Blink());
+            fill.color = highHealthMaterial.color;
+        }
+    }
+
+    private IEnumerator Blink()
+    {
+        float timer = 0f;
+        while (healthLow)
+        {
+            fill.color = lowHealthMaterial.color;
+            yield return null;
+
+            while(timer < 0.2f)
+            {
+                timer = timer + Time.deltaTime;
+                yield return null;
+            }
+            timer = 0f;
+
+            fill.color = highHealthMaterial.color;
+            yield return null;
+
+            while (timer < 0.2f)
+            {
+                timer = timer + Time.deltaTime;
+                yield return null;
+            }
+            timer = 0f;
+        }
     }
 }
