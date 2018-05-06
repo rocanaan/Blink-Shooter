b8fea1fd73f82359ea1194482f634b272257a590
brkdncr94 <bd1308@nyu.edu>
2018-05-06 12:05:02 -0400

    Healer Beacon Commit
    -Basic functionality of the healer beacons added. They now heal the boss in a given rate as long as they are alive, can be destroyed by the players.
    
    -TODO: Add visual feedback to show the players that the boss is being healed.

---
+++ Assets/HealorBeaconController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealorBeaconController : MonoBehaviour {
+
+    public int regenRate;
+    public float regenTime;
+
+    private GameObject boss;
+    private HealthController bossHealthController;
+    private ExplosionSpawner explosionSpawner;
+    private GameObject cam;
+
+    // Use this for initialization
+    void Start () {
+        explosionSpawner = transform.GetComponent<ExplosionSpawner>();
+        cam = GameObject.FindWithTag("MainCamera");
+        explosionSpawner.SpawnExplosion();
+        cam.GetComponent<CameraController>().CamShake(0.2f, 0.15f);
+
+        boss = GameObject.FindGameObjectWithTag("Boss");
+        bossHealthController = boss.GetComponent<HealthController>();
+
+        StartCoroutine(HealBoss());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private IEnumerator HealBoss()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(regenTime);
+            bossHealthController.Heal(regenRate);
+        }        
+    }
+}
