333ebadd3e951fa31132a58fd32ac87f23060974
Rodrigo Canaan <rodrigocanaan@rodrigos-mbp-2.home>
2018-05-08 22:46:22 -0400

    Made pause function also pause most sounds

---
+++ Assets/GhostBlinkOnActivation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostBlinkOnActivation : MonoBehaviour {
+
+	public float startDelay;
+	public float blinkDelay;
+	public float totalDuration;
+	public Material blinkMaterial;
+
+	private float nextStartTime;
+	private float nextBlinkTime;
+	private float nextEndTime;
+	private Material normalMaterial;
+	private SpriteRenderer sprite;
+
+	private bool isActive;
+
+	// Use this for initialization
+	void Start () {
+		isActive = false;
+		sprite = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isActive) {
+			if (Time.time > nextEndTime) {
+				isActive = false;
+				sprite.enabled = true;
+				sprite.material = normalMaterial;
+			} else if (Time.time > nextBlinkTime) {
+				sprite.enabled = true;
+				sprite.material = blinkMaterial;
+			} else if (Time.time >nextStartTime) {
+				sprite.enabled = true;
+				sprite.material = normalMaterial;
+			}
+		}
+	}
+
+	public void ScheduleBlinkOnActivation(){
+		isActive = true;
+		nextStartTime = Time.time + startDelay;
+		nextBlinkTime = Time.time + blinkDelay;
+		nextEndTime = Time.time + totalDuration;
+		normalMaterial = sprite.material;
+	}
+}
--- Assets/PauseGame.cs
+++ Assets/PauseGame.cs
@@ -4,20 +4,39 @@ using UnityEngine;
 
 public class PauseGame : MonoBehaviour {
 
+
+	private bool paused;
+
+	private GameObject audioSource;
+	private AudioController soundtrackController;
+
 	// Use this for initialization
 	void Start () {
-		
+		paused = false;
+
+		audioSource = GameObject.FindGameObjectWithTag ("GlobalAudioSource");
+		soundtrackController = audioSource.GetComponent<AudioController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetButtonDown("Pause"))
         {
-            if(Time.timeScale == 1.0F)
+            if(!paused)
             {
                 Time.timeScale = 0.0F;
-            }else { Time.timeScale = 1.0F; }
+				paused = true;
+				soundtrackController.GetComponent<AudioSource> ().Pause ();
+            }else { 
+				Time.timeScale = 1.0F;
+				paused = false;
+				soundtrackController.GetComponent<AudioSource> ().UnPause ();
+			}
         }
+	}
+
+	public bool isPaused(){
+		return paused;
 	}
 
 }
