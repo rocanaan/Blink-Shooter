39565e92530c16f54c3820ecf5f80a36f6bf9471
brkdncr94 <bd1308@nyu.edu>
2018-05-09 12:34:00 -0400

    Tutorial Start Animation Commit
    -Added starting screen to the first tutorial scene.

--- Assets/FirstTutorialGameController.cs
+++ Assets/FirstTutorialGameController.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class FirstTutorialGameController : MonoBehaviour {
 
 	public GameObject[] guns;
+    public Text gameOverText;
+    public Canvas canvas;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private Animator anim;
+    private bool gameOver;
+    private PauseGame pauseGame;
+
+    // Use this for initialization
+    void Start () {
+        Time.timeScale = 0f;
+        anim = canvas.GetComponent<Animator>();
+        gameOver = false;
+        pauseGame = GetComponent<PauseGame>();
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -21,12 +32,29 @@ public class FirstTutorialGameController : MonoBehaviour {
 				break;
 			}
 		}
+
+        if (Time.timeScale == 0f && Input.GetButton("Submit"))
+        {
+            print("trying to unpause");
+            Time.timeScale = 1f;
+            anim.SetTrigger("Start");
+            //anim.SetTrigger("GameOver");
+        }
+
 		if (gameOver) {
 			GameOver ();
 		}
+
+        if(gameOver && Input.GetButton("Submit"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
+        }
+
 	}
 
 	private void GameOver(){
-		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex+1,LoadSceneMode.Single);
+        gameOverText.text = "Congratulations! You Cleared The First Tutorial!";
+        anim.SetTrigger("GameOver");
+        
 	}
 }
